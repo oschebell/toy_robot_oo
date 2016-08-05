7f8ea52da617c3bc4434b227412e3ad5e282b2fb
oschebell <oschebell@gmail.com>
2016-08-05 16:56:03 +1000

    Refactor place method to use board class

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -2,61 +2,65 @@ require_relative 'board'
 
 class ToyRobot
 
-  def place(x, y, direction)
+  def place(x_coordinate, y_coordinate, direction)
     @board = Board.new
-    if @board.valid_placement?(x, y)
-      @x = x
-      @y = y
+    if @board.valid_placement?(x_coordinate, y_coordinate)
+      @x_coordinate = x_coordinate
+      @y_coordinate = y_coordinate
       @direction = direction
-    end
-  end
-
-  def move
-    if @x == nil
-      p "You cannot issue commands until the robot has been placed."
+      @placed
+      puts "Robot placed at coordinates x:#{x_coordinate}, y:#{y_coordinate} and facing #{direction}."
     else
-      if @direction == "EAST" && (@x + 1 < 6 )
-        @x = @x + 1
-      elsif @direction == "NORTH" && (@y + 1 < 6 )
-        @y = @y + 1
-      elsif @direction == "SOUTH" && (@y - 1 >= 0 )
-        @y = @y - 1
-      elsif @direction == "WEST" && (@x - 1 >= 0 )
-        @x = @x - 1
-      else
-        p "I'm sorry Dave, I can't do that..."
-      end
+      puts "Invalid placement. Please try again."
     end
   end
 
-  def left
-      if @direction == "NORTH"
-        @direction = "WEST"
-      elsif @direction == "WEST"
-        @direction = "SOUTH"
-      elsif @direction == "SOUTH"
-        @direction = "EAST"
-      elsif @direction == "EAST"
-        @direction = "NORTH"
-      end
-  end
-
-  def right
-    if @direction == "NORTH"
-      @direction = "EAST"
-    elsif @direction == "EAST"
-      @direction = "SOUTH"
-    elsif @direction == "SOUTH"
-      @direction = "WEST"
-    elsif @direction == "WEST"
-      @direction = "NORTH"
-    end
-  end
-
-  def report
-    p "#{@x}, #{@y}, #{@direction}"
-  end
-end
+  # def move
+  #   if @x && @y && @direction != nil
+  #     if @direction == "EAST" && @board.valid_placement?(@x+1, @y)
+  #       @x = @x + 1
+  #     elsif @direction == "NORTH" && @board.valid_placement?(@x, @y+1)
+  #       @y = @y + 1
+  #     elsif @direction == "SOUTH" && @board.valid_placement?(@x, @y-1)
+  #       @y = @y - 1
+  #     elsif @direction == "WEST" && @board.valid_placement?(@x-1, @y)
+  #       @x = @x - 1
+  #     else
+  #       p "I'm sorry Dave, I can't do that..."
+  #     end
+  #   else
+  #     p "You cannot issue commands until the robot has been placed."
+  #   end
+  # end
+  #
+  # def left
+  #     if @direction == "NORTH"
+  #       @direction = "WEST"
+  #     elsif @direction == "WEST"
+  #       @direction = "SOUTH"
+  #     elsif @direction == "SOUTH"
+  #       @direction = "EAST"
+  #     elsif @direction == "EAST"
+  #       @direction = "NORTH"
+  #     end
+  # end
+  #
+  # def right
+  #   if @direction == "NORTH"
+  #     @direction = "EAST"
+  #   elsif @direction == "EAST"
+  #     @direction = "SOUTH"
+  #   elsif @direction == "SOUTH"
+  #     @direction = "WEST"
+  #   elsif @direction == "WEST"
+  #     @direction = "NORTH"
+  #   end
+  # end
+  #
+  # def report
+  #   p "#{@x}, #{@y}, #{@direction}"
+  # end
+ end
 
 # toy_robot = ToyRobot.new
 # #toy_robot.place(0, 0, "NORTH")
@@ -91,7 +95,8 @@ end
 
 #d
 toy_robot = ToyRobot.new
-toy_robot.move
+toy_robot.place(0, 5, "NORTH")
+# toy_robot.move
 # toy_robot.left
 # toy_robot.left
 # toy_robot.place(0, 0, "NORTH")
