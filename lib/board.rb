c37f74e998fa95507c8de549f91ba6a8098f7181
oschebell <oschebell@gmail.com>
2016-08-05 16:55:30 +1000

    Implement board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,13 @@
 class Board
 
-  def valid_placement?(x, y)
-      x.between?(0, 5) && y.between?(0, 5)
+attr_reader :width, :height
+
+  def initialize
+    @width = 5
+    @height = 5
   end
 
-
+  def valid_placement?(x_coordinate, y_coordinate)
+    x_coordinate.between?(0, width) && y_coordinate.between?(0, height)
+  end
 end
